e55217dc46d22d12c8f0cd358c6a66bc2e8ca4bd
NISHALINI <nishalinithirukkumar2@gmail.com>
2024-10-17 21:40:33 +0530

    deploy

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
     cors({
-      origin: [process.env.CLIENT_URL ],
+      origin: [process.env.REACT_APP_BACKEND_URL ],
       methods: ["GET", "POST", "PUT", "DELETE"],
       credentials: true,
     })
